f0faae85c9be84c3d57054ae9c56cd00bd3b81e2
Hugo <hugo.rosenkranz@gmail.com>
2018-02-20 13:57:23 +0100

    Revert "."
    This reverts commit aec2b7435beafb68e46a48a1a3fdee3798e965e0.

--- Assets/Player/Script/Player.cs
+++ Assets/Player/Script/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     public float speed = 5.0f;
+    public float jumpSpeed = 2.5f;
 
     // Use this for initialization
     void Start () {
