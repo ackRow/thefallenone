559d826de70c3e4e1975280bd041b88faf0d4dbd
Creanaryte <32837490+Creanaryte@users.noreply.github.com>
2018-03-15 13:23:58 +0100

    Menu + Pause

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour {
+
+	public void Startbtn(string Scene)
+    {
+        SceneManager.LoadScene(Scene);
+    }
+
+    public void ExitBtn()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/PauseMenuScript.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour {
+
+    public GameObject menuObject;
+    public bool isActive = false;
+
+	// Update is called once per frame
+	void Update () {
+		
+        if (isActive)
+        {
+            menuObject.SetActive(true);
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.Confined;
+            Time.timeScale = 0;
+        }
+        else
+        {
+            menuObject.SetActive(false);
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            Time.timeScale = 1;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            RESUMEButton();
+        }
+	}
+
+    public void RESUMEButton()
+    {
+        isActive = !isActive;
+    }
+
+    public void MainMenuButton()
+    {
+        SceneManager.LoadScene("menutest");
+    }
+
+    public void EXITButton()
+    {
+        Application.Quit();
+    }
+}
