1d407b4d12a20753ea932c5a558296f82e6c739a
Hugo <hugo.rosenkranz@gmail.com>
2018-04-14 18:13:54 +0200

    Bot Routine Test

---
+++ Assets/PNJ/Script/BotController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotController : MonoBehaviour {
+
+    Bot bot;
+
+    bool hasRotate = true;
+    float angle;
+
+    int round = 50;
+
+    // Use this for initialization
+    void Start () {
+        bot = GetComponent<Bot>();
+
+        bot.Scope();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        bot.Forward(false, new Vector3(1.0f,0 ,0)); // go forward  
+
+    }
+
+    private void FixedUpdate()
+    {
+        // Routine
+        if (round == 100)
+        {
+            angle += 180.0f;
+
+            hasRotate = true;
+            bot.transform.rotation = Quaternion.AngleAxis(angle, bot.transform.up); // rotate
+
+            round = 0;
+        }
+
+        round++;
+    }
+}
