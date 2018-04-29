55020158fa4dd00138f98896e48d08258b34a6f4
Hugo <hugo.rosenkranz@gmail.com>
2018-04-29 20:24:27 +0200

    Box on Multi2

---
+++ Assets/Box/Online/Script/DamageBox_Net.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageBox_Net : Box_Net
+{
+
+    // Donne un gain de vie au joueur
+
+    public float damage = 25f;
+
+    public override void Action(Player_Net p)
+    {
+        p.TakeDamage(damage, "");
+        Destroy(gameObject);
+    }
+
+}
---
+++ Assets/Box/Online/Script/SpeedBox_Net.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedBox_Net : Box_Net
+{
+
+    public float speedValue = 10.0f;
+    public int duration = 10;
+
+    public override void Action(Player_Net p)
+    {
+        StartCoroutine(Effect(p));
+    }
+
+    IEnumerator Effect(Player_Net p)
+    {
+        p.running_speed += speedValue;
+        p.walking_speed += speedValue;
+
+        // Grisé
+
+        yield return new WaitForSeconds(duration);
+
+        p.running_speed -= speedValue;
+        p.walking_speed -= speedValue;
+
+        // Narmol
+        triggered = false;
+    }
+}
