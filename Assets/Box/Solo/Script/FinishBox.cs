8296b60fb16c771d2435a2739da40aa649c219f2
Hugo <hugo.rosenkranz@gmail.com>
2018-04-30 19:00:21 +0200

    lvl1 flag

--- Assets/Box/Solo/Script/FinishBox.cs
+++ Assets/Box/Solo/Script/FinishBox.cs
@@ -7,6 +7,13 @@ public class FinishBox : Box
     // Donne un gain de vie au joueur
 
     public int level = 1;
+
+    public new void Update()
+    {
+       // transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+
+    }
+
     public override void Action(Player p)
     {
         p.finishLevel(level);
