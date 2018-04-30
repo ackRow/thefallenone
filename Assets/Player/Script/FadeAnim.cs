39d7ad1307d9ab7d67fe3f8c0a1e97c9be252467
Creanaryte <32837490+Creanaryte@users.noreply.github.com>
2018-04-30 21:27:35 +0200

    BOX Fade effect
    ajout d'une classe public dans FadeAnim.cs pour l'animation des box, novueau script dans interface script pas encore implémenter

--- Assets/Player/Script/FadeAnim.cs
+++ Assets/Player/Script/FadeAnim.cs
@@ -11,6 +11,11 @@ public class FadeAnim : MonoBehaviour
     public Text deathtext;
     public RawImage hit_indicator;
     private float player_health;
+    public Text heal_box;
+    public Text speed_box;
+    public Text Coin_box;
+    public Text Wall_box;
+    public Text Damage_box;
 
     private void Awake()
     {
@@ -25,12 +30,43 @@ public class FadeAnim : MonoBehaviour
             hit_indicator.color = new Color(1, 1, 1, 1);
             StartCoroutine(FadeImage(true));
             player_health = healthbar.value;
+            hit_indicator.color = new Color(1, 1, 1, 0);
         }
 
         if (healthbar.value <= 0)
         {
-            StartCoroutine(FadeText(false));
-            StartCoroutine(FadeText(true));
+            StartCoroutine(FadeText(false, 5f, deathtext, 1, 0, 0));
+            StartCoroutine(FadeText(true, 5f, deathtext, 1, 0, 0));
+        }
+    }
+
+    public void GetBoxFade(string box_name)
+    {
+        switch (box_name)
+        {
+            case "heal":
+                StartCoroutine(FadeText(false, 1f, heal_box, 0, 1, 0));
+                StartCoroutine(FadeText(true, 1f, heal_box, 0, 1, 0));
+                break;
+            case "speed":
+                StartCoroutine(FadeText(false, 1f, speed_box, 0, 0, 1));
+                StartCoroutine(FadeText(true, 1f, speed_box, 0, 0, 1));
+                break;
+            case "wall":
+                StartCoroutine(FadeText(false, 1f, Wall_box, 0, 0, 1));
+                StartCoroutine(FadeText(true, 1f, Wall_box, 0, 0, 1));
+                break;
+            case "coin":
+                StartCoroutine(FadeText(false, 1f, Coin_box, 255, 213, 0));
+                StartCoroutine(FadeText(true, 1f, Coin_box, 255, 213, 0));
+                break;
+            case "damage":
+                StartCoroutine(FadeText(false, 1f, Damage_box, 1, 0, 0));
+                StartCoroutine(FadeText(true, 1f, Damage_box, 1, 0, 0));
+                break;
+            default:
+                Debug.Log("Wrong box name entered in parameter");
+                break;
         }
     }
 
@@ -55,22 +91,22 @@ public class FadeAnim : MonoBehaviour
         }
     }
 
-    IEnumerator FadeText(bool fadeAway)
+    IEnumerator FadeText(bool fadeAway, float duration, Text text, int red, int green, int blue)
     {
         if (fadeAway)
         {
-            for (float i = 5f; i >= 0; i -= Time.deltaTime)
+            for (float i = duration; i >= 0; i -= Time.deltaTime)
             {
-                deathtext.color = new Color(1, 0, 0, i);
+                text.color = new Color(red, green, blue, i);
                 yield return null;
             }
         }
 
         else
         {
-            for (float i = 5f; i <= 1; i += Time.deltaTime)
+            for (float i = duration; i <= 1; i += Time.deltaTime)
             {
-                deathtext.color = new Color(1, 0, 0, i);
+                text.color = new Color(red, green, blue, i);
                 yield return null;
             }
         }
---
+++ Assets/Scripts/Interface Script/HoverScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class HoverScript : MonoBehaviour, IPointerEnterHandler{
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
